dcc0d257b90f6cb86530307c545bf47e8a81f9fc
athirajohny <athira.mannapath@pearldatadirect.com>
2021-06-23 08:12:58 +0530

    api

--- lib/utility/api_provider.dart
+++ lib/utility/api_provider.dart
@@ -34,4 +34,32 @@ class ApiProvider{
     return responseData;
   }
 
+  Future<void> fetchPostData() async {
+    Map responseData;
+    try {
+      GraphQLConfiguration graphQLConfiguration1 = GraphQLConfiguration();
+      GraphQLClient _client1 = graphQLConfiguration1.clientToQuery();
+      var resultData = await _client1.mutate(MutationOptions(
+        document: gql(addMutation.post()),
+      ));
+      if (resultData.hasException) {
+        responseData = {"status": "error"};
+      } else {
+        if (resultData.data == null ||
+            resultData.data["megamenu"].length == 0) {
+          responseData = {"status": "empty"};
+        } else {
+          responseData = {
+            "status": "completed",
+            "value": resultData.data
+          };
+        }
+      }
+    } catch (e) {
+      responseData = {"status": "error"};
+      print(e);
+    }
+    return responseData;
+  }
+
 }
