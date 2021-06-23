574798c005aa6a751d6726075d0fbafd00bf817b
athirajohny <athira.mannapath@pearldatadirect.com>
2021-06-23 08:05:13 +0530

    fetchData

--- lib/utility/api_provider.dart
+++ lib/utility/api_provider.dart
@@ -1,6 +1,37 @@
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:post/utility/graphQl.dart';
 import 'package:post/utility/query_mutation.dart';
 
 class ApiProvider{
   QueryMutation addMutation = QueryMutation();
+
+
+  Future<void> fetchPost() async {
+    Map responseData;
+    try {
+      GraphQLConfiguration graphQLConfiguration1 = GraphQLConfiguration();
+      GraphQLClient _client1 = graphQLConfiguration1.clientToQuery();
+      var resultData = await _client1.mutate(MutationOptions(
+        document: gql(addMutation.postsQuery()),
+      ));
+      if (resultData.hasException) {
+        responseData = {"status": "error"};
+      } else {
+        if (resultData.data == null ||
+            resultData.data["megamenu"].length == 0) {
+          responseData = {"status": "empty"};
+        } else {
+          responseData = {
+            "status": "completed",
+            "value": resultData.data
+          };
+        }
+      }
+    } catch (e) {
+      responseData = {"status": "error"};
+      print(e);
+    }
+    return responseData;
+  }
 
 }
